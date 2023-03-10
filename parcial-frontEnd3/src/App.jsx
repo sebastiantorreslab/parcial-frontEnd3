317e74dd45991a11f879db5b565098562fd9bea2
sebastiantorreslab <str9371@gmail.com>
2023-03-09 20:25:07 -0500

    prueba repo

---
+++ parcial-frontEnd3/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Form } from './Components/Form'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Form/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ parcial-frontEnd3/src/Components/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card } from "./Card";
+
+export const Form = () => {
+  return (
+    <div>
+      Form
+      <Card />
+    </div>
+  );
+};
